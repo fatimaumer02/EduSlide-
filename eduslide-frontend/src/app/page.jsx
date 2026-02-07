b93811c8634e6ccd295b930ff9be0b0cf363b42c
fatimaumer02 <fatimaumer862@gmail.com>
2026-02-07 13:58:31 +0500

    Initial Next.js frontend setup

---
+++ eduslide-frontend/src/app/page.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-white font-sans text-black">
+     <p>HEllo World</p>
+    </div>
+  );
+}
